9bb06a9fb82754a1a72e904793c7d3c84f0c35d9
Muhammad Ibrahim Bello <kibrahimbello@yahoo.com>
2022-09-13 21:17:23 +0100

    Task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+/*
+ * File: 11-print_to_98.c
+ * Auth: Ebenezer Sam-Oladapo
+ */
+
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98.
+ * @n: parameter
+ * Return: void.
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i < 98; i++)
+		_putchar("%i, ", i);
+	for (i = n; i > 98; i--)
+		_putchar("%i, ", i);
+	_putchar("98\n");
+}
