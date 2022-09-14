b61ff06778fb6455724be7094fe30ba6a8fc7ba5
Keenmuhammad <kibrahimbello@yahoo.com>
2022-09-14 08:52:37 +0100

    Task 13

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,21 +12,21 @@ int main(void)
 
 {
 
-    int x = 1024, y, sum = 0;
+        int x = 1024, y, sum = 0;
 
-    for (y = 0; y < x; y++)
-
-    {
-
-        if ((y % 3 == 0) || (y % 5 ==0))
+        for (y = 0; y < x; y++)
 
         {
 
-            sum = sum + y;
+                if ((y % 3 == 0) || (y % 5 ==0))
+
+            {
+
+                        sum = sum + y;
+
+            }
 
         }
-
-    }
 
     printf("%d\n", sum);
 
