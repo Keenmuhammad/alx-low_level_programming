d9234aa760666a1876a926cae4454e1b7cdca295
Muhammad Ibrahim Bello <kibrahimbello@yahoo.com>
2022-09-19 15:42:14 +0000

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,15 @@
 #include "main.h"
+
 /**
- *_puts - a function that prints a string, followed
- * by a new line, to stdout.
- * @str: an input strig
- * Return: Nothing
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	char *str;
-	
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	
-	_puts(str);
-	
-	return (0);
+  char *str;
+
+  str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+  _puts(str);
+  return (0);
 }
