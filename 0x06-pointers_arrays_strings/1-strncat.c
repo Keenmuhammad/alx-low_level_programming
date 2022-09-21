33ded3475f7a0c6cf06970e4503d5b6acc6257a5
Muhammad Ibrahim Bello <kibrahimbello@yahoo.com>
2022-09-21 20:01:03 +0000

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,26 +1,21 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates n bytes from a string to another
+ * _strncat - concatenates string with n bytes from another string
  * @dest: destination string
  * @src: source string
- * @n: number of bytes of str to concatenate
- * Return: a pointer to the resulting string dest
+ * @n: bytes to be copied
+ * Return: destination string concatenated
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-int i, j;
-i = 0;
-j = 0;
-while (dest[i] != '\0')
-i++;
-while (src[j] != '\0' && j < n)
-{
-dest[i] = src[j];
-i++;
-j++;
-}
-dest[i] = '\0';
-return (dest);
+	int i = 0, j;
+
+	while (dest[i] != '\0')
+		i++;
+	for (j = 0; j < n && src[j] != '\0'; j++, i++)
+		dest[i] = src[j];
+	dest[i] = '\0';
+	return (dest);
 }
