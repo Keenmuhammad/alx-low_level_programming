1c6149abadf0cfbb48b039ab6432a1e7ff332d6f
Muhammad Ibrahim Bello <kibrahimbello@yahoo.com>
2022-09-22 10:08:28 +0100

    Task 11

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,51 +1,54 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
- * infinite_add - adds values of two strings together
- * @n1: first string of integers
- * @n2: second string of ints
- * @r: result string
- * @size_r: size of result string
- * Return: void
+ * infinite_add - adds two numbers
+ * @n1: number one.
+ * @n2: number two.
+ * @r: buffer that the function will use to store the result.
+ * @size_r: buffer size:
+ * Return: the pointer to dest.
  */
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0, num1 = 0, sum1 = 0;
+	int c1 = 0, c2 = 0, op, bg, dr1, dr2, add = 0;
 
-	int len2 = 0, num2 = 0, sum2 = 0;
-
-	int num = 0, sum = 0, i, j;
-
-	char temp, n1c[1000], n2c[1000];
-
-	while (n1[len1] != '\0')
-		len1++;
-	while (n2[len2] != '\0')
-		len2++;
-	if (len1 >= size_r || len2 >= size_r)
+	while (*(n1 + c1) != '\0')
+		c1++;
+	while (*(n2 + c2) != '\0')
+		c2++;
+	if (c1 >= c2)
+		bg = c1;
+	else
+		bg = c2;
+	if (size_r <= bg + 1)
 		return (0);
-	for (j = 0, i = len1 - 1; j < len1; j++, i--)
-		n1c[j] = n1[i];
-	n1c[j] = '\0';
-	for (j = 0, i = len2 - 1; j < len2; j++, i--)
-		n2c[j] = n2[i];
-	n2c[j] = '\0';
-	while (num < size_r - 1)
+	r[bg + 1] = '\0';
+	c1--, c2--, size_r--;
+	dr1 = *(n1 + c1) - 48, dr2 = *(n2 + c2) - 48;
+	while (bg >= 0)
 	{
-		if (num1 >= len1 && num2 >= len2 && sum == 0)
-			break;
-		sum1 = num1 < len1 ? n1c[num1] - '0' : 0;
-		sum2 = num2 < len2 ? n2c[num2] - '0' : 0;
-		sum += (sum1 + sum2);
-		r[num] = sum % 10 + '0';
-		sum = sum < 10 ? 0 : 1;
-		num++, num1++, num2++;
+		op = dr1 + dr2 + add;
+		if (op >= 10)
+			add = op / 10;
+		else
+			add = 0;
+		if (op > 0)
+		*(r + bg) = (op % 10) + 48;
+		else
+			*(r + bg) = '0';
+		if (c1 > 0)
+			c1--, dr1 = *(n1 + c1) - 48;
+		else
+			dr1 = 0;
+		if (c2 > 0)
+			c2--, dr2 = *(n2 + c2) - 48;
+		else
+			dr2 = 0;
+		bg--, size_r--;
 	}
-	if (sum != 0)
-		return (0);
-	r[num] = '\0';
-	for (j = 0, i = --num; j < num / 2; j++, i--)
-		temp = r[j], r[j] = r[i], r[i] = temp;
-	return (r);
+	if (*(r) == '0')
+		return (r + 1);
+	else
+		return (r);
 }
