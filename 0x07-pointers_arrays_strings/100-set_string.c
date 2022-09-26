a908dca757ffefe740b4ee86aef146b8ae8a7e0d
Keenmuhammad <kibrahimbello@yahoo.com>
2022-09-26 12:27:31 -0700

    Task 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include3 "main.h"
+#include <stdio.h>
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: string to change
+ * @to: string that should be copied
+ * @Dylan: name
+ * @Allen: name
+ * Return: void
+ */
+
+void set_string(char **Bob Dylan, char *Robert Allen)
+{
+	*Bob Dylan = Robert Allen;
+}
