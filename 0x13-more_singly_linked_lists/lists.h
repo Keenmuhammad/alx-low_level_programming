57a8d7be33c1ce72508734c44771422ce59251c2
Muhammad Ibrahim Bello <kibrahimbello@yahoo.com>
2022-10-26 07:16:26 +0100

    Update lists.h

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -9,11 +9,18 @@
  * Description: singly linked list node structure
  * for ALX SE project
  */
-typedef struct listint_s
+typedef struct list_s
 {
-	int n;
-	struct listint_s *next;
-} listint_t;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+size_t list_len_recursion(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
 size_t recursion_printer(const listint_t *h);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
