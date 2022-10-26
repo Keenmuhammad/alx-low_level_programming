9cd301dd3a9a1bc9d20c6dd299d9f6c1d68020d5
Muhammad Ibrahim Bello <kibrahimbello@yahoo.com>
2022-10-26 06:36:05 +0100

    Update 2-add_nodeint.c

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -6,16 +6,17 @@
  * @head: A pointer to list_t structure
  * @n: An integer data to put into the new node
  * Return: the address of the new element, or NULL on failure
+ * Muhammad = New node
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_node;
+	listint_t *Muhammad;
 
-	new_node = *head;
+	Muhammad = *head;
 	*head = malloc(sizeof(listint_t));
 	if (!*head)
 		return (NULL);
 	(*head)->n = n;
-	(*head)->next = new_node;
+	(*head)->next = Muhammad;
 	return (*head);
 }
