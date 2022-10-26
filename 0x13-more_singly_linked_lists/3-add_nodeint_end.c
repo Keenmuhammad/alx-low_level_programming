66a75f60e154a2e00409e94559ca81677f014f55
Keenmuhammad <kibrahimbello@yahoo.com>
2022-10-25 23:00:29 -0700

    Fix

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,32 +1,40 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - A function that adds a new node to the end of list
- * @head: A pointer to list structure
- * @n: An integer data to put into the new node
- * Return: The address of the new element, or NULL on failure
+ * add_nodeint_end - add a new node at the end
+ * of a linked list
+ * @head: head of a list.
+ * @n: n element.
+ *
+ * Return: address of the new element. NUll if it failed.
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new_node, *temp;
+	listint_t *Muhammad;
+	listint_t *Ibrahim;
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	(void)Ibrahim;
+
+	Muhammad = malloc(sizeof(listint_t));
+
+	if (Muhammad == NULL)
 		return (NULL);
 
-	new_node->n = n;
+	Muhammad->n = n;
+	Muhammad->next = NULL;
+	Ibrahim = *head;
 	if (*head == NULL)
 	{
-		new_node->next = *head;
-		*head = new_node;
+		*head = Muhammad;
 	}
 	else
 	{
-		new_node->next = NULL;
-		temp = *head;
-		while (temp->next)
-			temp = temp->next;
-		temp->next = new_node;
+		while (Muhammad->next != NULL)
+		{
+			Ibrahim = Ibrahim->next;
+		}
+		Muhammad->next = Ibrahim;
 	}
-	return (new_node);
+
+	return (*head);
 }
